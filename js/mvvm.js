591f28c9f15d8e19b003dd96b8cf8d09d5c1c480
summerbaby <596804755@qq.com>
2019-02-20 19:59:05 +0800

    feat: mvvm

---
+++ js/mvvm.js
@@ -0,0 +1,53 @@
+function MVVM(options) {
+    this.$options = options || {};
+    var data = this._data = this.$options.data; // 获取 Data 数据
+    var me = this;
+
+    // 数据代理
+    // 实现 vm.xxx -> vm._data.xxx
+    Object.keys(data).forEach(function(key) {
+        me._proxyData(key);
+    });
+
+    this._initComputed(); // 定义 computed 的 数据的 set 方法
+
+    observe(data, this);  // 监听所有Data 的变化
+
+    this.$compile = new Compile(options.el || document.body, this)
+}
+
+MVVM.prototype = {
+    $watch: function(key, cb, options) {
+        new Watcher(this, key, cb);
+    },
+
+    _proxyData: function(key, setter, getter) {
+        var me = this;
+        setter = setter || 
+        Object.defineProperty(me, key, {
+            configurable: false,
+            enumerable: true,
+            get: function proxyGetter() {
+                return me._data[key];
+            },
+            set: function proxySetter(newVal) {
+                me._data[key] = newVal;
+            }
+        });
+    },
+
+    _initComputed: function() {
+        var me = this;
+        var computed = this.$options.computed;
+        if (typeof computed === 'object') {
+            Object.keys(computed).forEach(function(key) {
+                Object.defineProperty(me, key, {
+                    get: typeof computed[key] === 'function' 
+                            ? computed[key] 
+                            : computed[key].get,
+                    set: function() {}
+                });
+            });
+        }
+    }
+};
